312edf0766d0c1bf7a59092bdfd1d0e97e892fbe
Yuxiang Zhu <yz7043@nyu.edu>
2022-05-26 22:55:40 -0400

    menu dev

--- djangoGame/game/static/js/src/menu/zbase.js
+++ djangoGame/game/static/js/src/menu/zbase.js
@@ -3,8 +3,48 @@ class MainGameMenu{
         this.root = root;
         // for an html object, we usually add $ before its name in jQuery
         this.$menu = $(`
-            <div class="game_menu"></div>
+            <div class="game_menu">
+            <div class="game_menu_field">
+                <div class="game_menu_field_item game_menu_field_item_solo_mode">
+                    Solo Mode
+                </div>
+                <br/>
+                <div class="game_menu_field_item game_menu_field_item_multi_mode">
+                    Party Mode
+                </div>
+                <br/>
+                <div class="game_menu_field_item game_menu_field_item_setting_mode">
+                    Settings
+                </div>
+            </div>
+            </div>
             `);
         this.root.$game.append(this.$menu);
+        this.$single_mode = this.$menu.find('.game_menu_field_item_solo_mode');
+        this.$multi_mode = this.$menu.find('.game_menu_field_item_multi_mode');
+        this.$setting = this.$menu.find('.game_menu_field_item_setting_mode');
+
+        this.start();
+    }
+
+    start(){
+        this.add_listening_events();
+    }
+
+    add_listening_events(){
+        let outer = this;
+        this.$single_mode.click(function(){
+            // inner function's this is different from the class this
+            console.log("click solo mode");
+        });
+
+        this.$multi_mode.click(function(){
+            // inner function's this is different from the class this
+            console.log("click multi mode");
+        });
+
+        this.$setting.click(function(){
+            console.log("click setting button");
+        });
     }
 }
